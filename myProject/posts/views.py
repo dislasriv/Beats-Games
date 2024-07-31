9a711648023b7311f480dd05fcf8163e9cc4640a
Diego Islas <boogeyif@gmail.com>
2024-07-30 20:15:50 -0700

    Day 4: Templates, URLS, slugs; adding images
    Learned to setup media folder and add image fields to models, learned how to access image fields in HTML, learned to use path converters to allow one URL to cover many instances of a model being rendered to a template.
    
    learned how named URLS can be accessed from HTML and have parameters passed into them, then into the path() function and finally into the view function that renders a particular template. We can use these parameters for tons of things, including rendering the content of a particular model instance on an HTML page (if you pass it into the context parameter of render() in the view function)

--- myProject/posts/views.py
+++ myProject/posts/views.py
@@ -4,3 +4,9 @@ from .models import Post
 # Create your views here.
 def posts_list(request):
     return render(request, 'posts/posts_list.html', {'posts':Post.objects.all().order_by("-date")})
+
+
+def post_page(request, slug):
+    posts = Post.objects.all().order_by("-date")
+    #get post with particular slug, send it as the post we want to represent in the post template
+    return render(request, 'posts/post_page.html',{"this_post":posts.get(slug=slug),})
