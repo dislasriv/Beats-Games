2d8ee5af0898c6311f4447ba5a98d244d303e4fb
Diego Islas <boogeyif@gmail.com>
2024-08-02 21:02:55 -0700

    Day 6: Authorization, Custom Forms, End of tutorial series HERE.
    Django logout, @login_required(): authorizing certain pages for logged in users only, HTTPRequest.GET.next, Django ModelForms (writing, pasting into HTML, extracting models from them in view functions), form.save(commit = false), intro to relational databases.

--- myProject/posts/urls.py
+++ myProject/posts/urls.py
@@ -6,5 +6,6 @@ app_name = "posts"
 
 urlpatterns = [
     path('', views.posts_list, name="list"),
+    path('new-post', views.new_post, name="new-post"),
     path('<slug:slug>', views.post_page, name="page"),
 ]
