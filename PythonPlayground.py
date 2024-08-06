7a0b78baba0b8db1fadca0c3a92313058316f2ee
Diego Islas <boogeyif@gmail.com>
2024-08-05 18:17:01 -0700

    End of day: Find a way to get SpotifyAPI authorization so we can import playlists to the program.

--- PythonPlayground.py
+++ PythonPlayground.py
@@ -1,12 +1,10 @@
+import requests
+
 def main():
-    poop
-    print("Hello World!")
-
-
-def poop(n):
-    print("i am sad")
-
+    busa = requests.get("https://api.spotify.com/v1/playlists/20g1GYEqzeiD8IMpSMj21J")
+    print("CHESTO")
+    print(busa)
 
 if __name__ == "__main__":
     main()
-    5
+
--- myProject/posts/models.py
+++ myProject/posts/models.py
@@ -3,15 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Playlist(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, default = None)
     title = models.CharField(max_length=50)
     body = models.TextField()
     banner = models.ImageField(default="fallback.jpg", blank=True)
+    playlistId = models.CharField(max_length=200, default = "ERROR")
 
     #static fields
     slug = models.SlugField()
     date = models.DateTimeField(auto_now_add=True)
-   
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default = None)
 
     def __str__(self):
         return "Post ToString:" + self.title
