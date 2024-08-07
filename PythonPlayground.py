05fc72a473638fd376ec1288dae57aa63ae4b7c1
Diego Islas <boogeyif@gmail.com>
2024-08-07 16:07:21 -0700

    Spotipy Setup

--- PythonPlayground.py
+++ PythonPlayground.py
@@ -1,9 +1,20 @@
 import requests
+import spotipy
+import sys
+from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
+
+
+scope = 'playlist-read-private'
 
 def main():
-    busa = requests.get("https://api.spotify.com/v1/playlists/20g1GYEqzeiD8IMpSMj21J")
-    print("CHESTO")
-    print(busa)
+    spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+    
+    try:
+        playlist = spotify.playlist("20g1GYEqzeiD8IMpSMj21J")
+        return playlist
+    except spotipy.exceptions.SpotifyException as e:
+        print(f"Error: {e}")
+        return None
 
 if __name__ == "__main__":
     main()
--- SpotifyApiReference/main.py
+++
@@ -1,6 +0,0 @@
-from dotenv import *
-import os
-
-load_dotenv()
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
